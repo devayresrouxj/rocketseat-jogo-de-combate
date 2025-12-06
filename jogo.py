b18048dc51513609b5c127bc8074c603abd1dae4
Ayres Roux <devayresrouxj@gmail.com>
2025-12-06 06:18:35 -0300

    feat: criando o orquestrador de combate

--- jogo.py
+++ jogo.py
@@ -1,3 +1,6 @@
+# Personagem: classe mae
+# Heroi: controlado pelo usuario
+# Inimigo: adversario do usuario
 class Personagem:
   def __init__(self, nome, vida, nivel):
     self.__nome = nome
@@ -38,8 +41,23 @@ class Inimigo(Personagem):
   def exibir_detalhes(self):
     return f"{super().exibir_detalhes()}\nTipo: {self.get_tipo()}\n"
   
-heroi = Heroi(nome="Herói", vida=100, nivel=5, habilidade="Super Força")
-print(heroi.exibir_detalhes())
+class Jogo:
+  """Classe orquestradora do jogo"""
+  def __init__(self):
+    self.heroi = Heroi(nome="Herói", vida=100, nivel=5, habilidade="Super Força")
+    self.inimigo = Inimigo(nome="Morcego", vida=50, nivel=3, tipo="Voador")
+  
+  def iniciar_batalha(self):
+    """Fazer a gestão da batalha em turnos"""
+    print("Inciando batalha!")
+    while self.heroi.get_vida() > 0 and self.inimigo.get_vida() > 0:
+      print("\nDetalhes dos Personagens:")
+      print(self.heroi.exibir_detalhes())
+      print(self.inimigo.exibir_detalhes())
 
-inimigo = Inimigo(nome="Morcego", vida=50, nivel=3, tipo="Voador")
-print(inimigo.exibir_detalhes())
+      input("Pressione para enter para atacar...")
+      escolha = input("Escolha (1 - Ataque Normal, 2 - Ataque Especial): ")
+
+# Criar instância do jogo e iniciar batalha
+jogo = Jogo()
+jogo.iniciar_batalha()
